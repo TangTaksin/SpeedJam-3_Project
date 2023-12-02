028d8653d1bcd528dcd1f30f3f52fe2d6b06ed70
Daggoot <taksin_tang51@hotmail.com>
2023-12-02 09:14:05 +0700

    Test Script and Commit

--- Assets/Scripts/ScriptTest.cs
+++ Assets/Scripts/ScriptTest.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class ScriptTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Hello Unity");
         
     }
 
